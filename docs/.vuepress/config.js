ffff8aa4750f3e2dcace94c3394cbcc13dfc2ec0
lg <245249531@qq.com>
2020-03-28 17:02:18 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,24 +9,24 @@ module.exports = {
         lineNumbers: true
     },
     serviceWorker: true,
-    themeConfig: {    
+    themeConfig: {
         logo: '/egg.png',
         lastUpdated: 'lastUpdate', // string | boolean
         nav: [
             { text: '首页', link: '/' },
             {
-                text: '分类',
-                ariaLabel: '分类',
+                text: '笔记',
+                ariaLabel: '笔记',
                 items: [
                     { text: '文章', link: '/pages/folder1/test1.md' },
                     { text: '琐碎', link: '/pages/folder2/test4.md' },
                 ]
             },
             { text: '功能演示', link: '/pages/folder1/test3.md' },
-            { text: 'Github', link: 'https://github.com/dwanda' },
+            { text: 'Github', link: 'https://github.com/alternativedestiny' },
         ],
         sidebar: {
-            '/pages/folder1/':[
+            '/pages/folder1/': [
                 {
                     title: '测试菜单1',   // 必要的
                     collapsable: false, // 可选的, 默认值是 true,
