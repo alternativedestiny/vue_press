8faab34047aab1facc4505caa61273d9dbf772bf
lg <245249531@qq.com>
2020-03-28 16:25:25 +0800

    test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    title: '裂泉首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    description: '裂泉的前端记录', // meta 中的描述文字，用于SEO
+    // 注入到当前页面的 HTML <head> 中的标签
+    head: [
+        ['link', { rel: 'icon', href: '/egg.png' }],  //浏览器的标签栏的网页图标
+    ],
+    markdown: {
+        lineNumbers: true
+    },
+    serviceWorker: true,
+    themeConfig: {    
+        logo: '/egg.png',
+        lastUpdated: 'lastUpdate', // string | boolean
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '分类',
+                ariaLabel: '分类',
+                items: [
+                    { text: '文章', link: '/pages/folder1/test1.md' },
+                    { text: '琐碎', link: '/pages/folder2/test4.md' },
+                ]
+            },
+            { text: '功能演示', link: '/pages/folder1/test3.md' },
+            { text: 'Github', link: 'https://github.com/dwanda' },
+        ],
+        sidebar: {
+            '/pages/folder1/':[
+                {
+                    title: '测试菜单1',   // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+                        ['test1.md', '子菜单1'],
+                        ['test3.md', '子菜单2']
+                    ]
+                },
+                {
+                    title: '测试菜单2',
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        ['test2.md', '子菜单1']
+                    ]
+                }
+            ],
+        }
+    }
+}
